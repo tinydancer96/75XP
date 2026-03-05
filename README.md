0231a75a0af5de90797298fe8e8641d15f38d4d8
Nasif Islam <178942734+Nasif-Islam@users.noreply.github.com>
2026-03-05 12:19:04 +0000

    add documentation for CLOUDFARE API

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ A mobile app that helps users track their daily habits during the 75 Hard challe
 [View Project Presentation](https://www.canva.com/design/DAHC4xi2_tU/otKEfyHXPk4Bz2FXZ4S4sA/edit?utm_content=DAHC4xi2_tU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)
 
 [Research Document](./Planning/RESEARCH.md)
-
-[Planning Document](./Planning/PLANNING.md)
--- spiking/ai-summary/cloudfare/index.js
+++ spiking/ai-summary/cloudfare/index.js
@@ -1,22 +1,30 @@
-import base_prompt from '../base_prompt.js';
-import data from '../request.js';
+import base_prompt from "../base_prompt.js";
+import data from "../request.js";
+import dotenv from "dotenv";
 
-const ACCOUNT_ID = '';
-const API_TOKEN = '';
-const MODEL = '@cf/meta/llama-3.1-8b-instruct';
+dotenv.config({ path: `.env` });
+
+const ACCOUNT_ID = process.env.CLOUDFLARE_ACCOUNT_ID;
+const API_TOKEN = process.env.CLOUDFLARE_API_TOKEN;
+const MODEL = process.env.CLOUDFLARE_MODEL;
+
+if (!API_TOKEN || !ACCOUNT_ID || !MODEL) {
+  console.error("Missing environment variables");
+  process.exit();
+}
 
 const response = await fetch(
   `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/ai/run/${MODEL}`,
   {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${API_TOKEN}`
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_TOKEN}`,
     },
     body: JSON.stringify({
-      prompt: `${base_prompt} ${data}`
-    })
-  }
+      prompt: `${base_prompt} ${data}`,
+    }),
+  },
 );
 
 const output = await response.json();
