72834723d5a460c430f25a09ed9b03adf5f938b3
medua3 <medua443@gmail.com>
2026-03-12 14:07:21 +0000

    Fixed return syntax

--- frontend/app/(tabs)/Profile.jsx
+++ frontend/app/(tabs)/Profile.jsx
@@ -1,9 +1,12 @@
-import { Text,View } from "react-native";
+import { Text, View } from "react-native";
 import Achievements from "../components/Achievements";
 export default function Profile() {
-  return   <View><Text>Profile Page</Text>
- <View>
-  <Achievements/>
-</View> </View>
- ;
+  return (
+    <View>
+      <Text>Profile Page</Text>
+      <View>
+        <Achievements />
+      </View>{" "}
+    </View>
+  );
 }
