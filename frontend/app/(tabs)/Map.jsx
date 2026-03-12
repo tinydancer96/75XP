1d8b746243483a0a6157290c1a4049816446533a
tinydancer96 <mardgelleneri@gmail.com>
2026-03-12 01:08:31 +0000

    added grid layout. will map out the node positions next. currently have 75 rows in gridItems array to print each item onto grid. will need to make component to clean up code

--- frontend/app/(tabs)/Map.jsx
+++ frontend/app/(tabs)/Map.jsx
@@ -1,20 +1,119 @@
-import { Text, ImageBackground, StyleSheet, ScrollView, Dimensions, FlatList } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, StyleSheet, Dimensions, ImageBackground, ScrollView } from "react-native";
 
-const { width, height } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 
-export default function Map() {
-  const image = {
-    uri: "https://i.pinimg.com/1200x/a5/2a/1c/a52a1c08cec6578928dd2d840c66a843.jpg",
-  };
+const columns = 20;
+const rows = 35;
+
+const imgWidth = width;
+const imgHeight = width * 2;
+
+const cellWidth = imgWidth / columns;
+const cellHeight = imgHeight / rows;
+
+const toCell = (row, col) => ({
+  position: "absolute",
+  left: (col - 1) * cellWidth,
+  top: (row - 1) * cellHeight,
+  width: cellWidth,
+  height: cellHeight,
+});
+
+const gridItems = [
+  { row: 30, col: 3, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+  { row: 25, col: 4, component: <View style={{ flex: 1, backgroundColor: "red" }} /> },
+  { row: 22, col: 5, component: <View style={{ flex: 1, backgroundColor: "blue" }} /> },
+  { row: 22, col: 7, component: <View style={{ flex: 1, backgroundColor: "green" }} /> },
+];
+
+const image = {
+  uri: "https://i.pinimg.com/1200x/a5/2a/1c/a52a1c08cec6578928dd2d840c66a843.jpg",
+};
+
+export default function GridOverlay() {
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-          <FlatList numColumns={15}></FlatList>
-        </ImageBackground>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView style={styles.container} contentContainerStyle={styles.content}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View style={StyleSheet.absoluteFill} pointerEvents="box-none">
+          {gridItems.map(({ row, col, component }, index) => (
+            <View key={index} style={toCell(row, col)}>
+              {component}
+            </View>
+          ))}
+        </View>
+      </ImageBackground>
+    </ScrollView>
   );
 }
 
@@ -22,8 +121,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  content: {
+    flexGrow: 1,
+  },
   image: {
-    width: width,
-    aspectRatio: 1200 / 2000,
+    width: imgWidth,
+    height: imgHeight,
   },
 });
