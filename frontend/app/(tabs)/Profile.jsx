b7a6e49ffdc08de539df1f274de5877d9d23aa0c
medua3 <medua443@gmail.com>
2026-03-13 15:06:37 +0000

    Working display of a Mood chart

--- frontend/app/(tabs)/Profile.jsx
+++ frontend/app/(tabs)/Profile.jsx
@@ -1,10 +1,14 @@
 import { Text, View } from "react-native";
 import Achievements from "../components/Achievements";
+import MoodChart from "../components/MoodChart";
 export default function Profile() {
   return (
     <View>
       <View>
         <Achievements />
+      </View>
+      <View>
+        <MoodChart />
       </View>
     </View>
   );
