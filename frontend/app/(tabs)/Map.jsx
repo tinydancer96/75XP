6e11613303f8460e0d66bbc66234cee97a2de21c
tinydancer96 <mardgelleneri@gmail.com>
2026-03-17 21:38:03 +0000

    mapped out the nodes

--- frontend/app/(tabs)/Map.jsx
+++ frontend/app/(tabs)/Map.jsx
@@ -1,15 +1,19 @@
-import { View, StyleSheet, Dimensions, ImageBackground, ScrollView, Pressable } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+  ScrollView,
+  Pressable,
+} from "react-native";
 
 import MapNodes from "../components/MapNodes";
 
+const image = require("../assets/mountain-peak-with-path-for-a-mobile-game (1).jpeg");
 const { width } = Dimensions.get("window");
 
 const imgWidth = width;
-const imgHeight = width * 2;
-
-const image = {
-  uri: "https://i.pinimg.com/1200x/a5/2a/1c/a52a1c08cec6578928dd2d840c66a843.jpg",
-};
+const imgHeight = width * 7;
 
 export default function GridOverlay() {
   return (
